d0ab8bedbbfc1ed7031338c4fd6b376f423a6467
mjlopez <maximilianojlopez@hotmail.com>
2025-04-24 12:27:44 -0300

    150

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: "basic",
+    title: "pipes básicos",
+    loadComponent: () => import("./pages/basic-page/basic-page.component")
+  },
+  {
+    path: "numbers",
+    title: "numbers básicos",
+    loadComponent: () => import("./pages/number-page/number-page.component")
+  },
+  {
+    path: "uncommon",
+    title: "pipes no tan comunes",
+    loadComponent: () => import("./pages/uncommon-page/uncommon-page.component")
+  },
+  {
+    path: "custom",
+    title: "pipes personalizados",
+    loadComponent: () => import("./pages/custom-page/custom-page.component")
+  },
+  {
+    path: "**",
+    redirectTo: "basic",
+  }
+
+
+];
