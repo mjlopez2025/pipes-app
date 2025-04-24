e9185b32a1174ab03b2e9b715e907dd16eeb275d
mjlopez <maximilianojlopez@hotmail.com>
2025-04-24 14:12:25 -0300

    152

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
   {
-    path: "basic",
-    title: "pipes básicos",
+    path: "Basic",
+    title: "Pipes básicos",
     loadComponent: () => import("./pages/basic-page/basic-page.component")
   },
   {
     path: "numbers",
-    title: "numbers básicos",
+    title: "Numbers Pipe",
     loadComponent: () => import("./pages/number-page/number-page.component")
   },
   {
     path: "uncommon",
-    title: "pipes no tan comunes",
+    title: "Pipes no tan comunes",
     loadComponent: () => import("./pages/uncommon-page/uncommon-page.component")
   },
   {
     path: "custom",
-    title: "pipes personalizados",
+    title: "Pipes Personalizados",
     loadComponent: () => import("./pages/custom-page/custom-page.component")
   },
   {
--- src/app/pages/number-page/number-page.component.ts
+++ src/app/pages/number-page/number-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './number-page.component.html',
 })
-export default class NumberPageComponent { }
+export default class NumberPageComponent {
+
+  totalSells = signal(2_433_232.5567)
+  percent = signal(1.4856)
+
+
+}
