625c027552970bba8ee76d61a948e3b3fcd0458c
mjlopez <maximilianojlopez@hotmail.com>
2025-04-24 14:50:15 -0300

    153

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 
 })
@@ -14,4 +14,16 @@ export default class BasicPageComponent {
   fullName = signal('MaximIliAno lOPez')
 
 
- }
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+
+    const interval = setInterval(() => {
+    this.customDate.set(new Date())
+  }, 1000);
+
+  onCleanup(() => {
+    clearInterval(interval);
+  })
+ })
+}
