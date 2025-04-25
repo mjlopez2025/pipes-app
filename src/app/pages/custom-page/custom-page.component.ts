287ee6668201f7c59c0b82dd60e18d2b72a77d80
mjlopez <maximilianojlopez@hotmail.com>
2025-04-25 12:51:47 -0300

    171

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from "../../data/heroes.data";
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
