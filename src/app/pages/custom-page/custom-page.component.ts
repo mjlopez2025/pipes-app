1dfac4d4ad5d1fad07824db925b7a40689585489
mjlopez <maximilianojlopez@hotmail.com>
2025-04-25 15:01:59 -0300

    173

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -5,10 +5,12 @@ import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/heroColor.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { HeroCreatorPipe } from "../../pipes/hero-creator.pipe";
+import { HeroSortByPipe} from "../..//pipes/hero-short-by.pipe";
 import { TitleCasePipe } from '@angular/common';
+import { Hero } from '../../interfaces/hero.interface';
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -17,4 +19,6 @@ export default class CustomPageComponent {
   upperCase = signal(true);
 
   heroes = signal(heroes);
+
+  sortBy = signal<keyof Hero | null>(null)
  }
